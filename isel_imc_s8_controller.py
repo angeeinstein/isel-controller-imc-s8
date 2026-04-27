a663db3c1395870e361f9b907c136cf22519cad6
Ange Einstein <ichfrissdich@gmail.com>
2026-04-27 19:55:29 +0200

    Update API server host to allow external connections

--- isel_imc_s8_controller.py
+++ isel_imc_s8_controller.py
@@ -1850,7 +1850,7 @@ if __name__ == "__main__":
     controller = IselController()
     
     # Start TCP Server on port 5000 for MATLAB communication
-    api = ApiServer(controller, host='127.0.0.1', port=5000)
+    api = ApiServer(controller, host='0.0.0.0', port=5000)
     api.start()
 
     # Start Main GUI
